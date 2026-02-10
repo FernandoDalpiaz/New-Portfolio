b97478d84a2045ca72727b39dfb843245686d35b
Fernando Dalpiaz <fernandoldalpiaz@gmail.com>
2026-02-10 18:31:49 -0300

    feat: Alternar entre tema claro e escuro

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,21 +1,47 @@
 import styles from './styles.module.css';
 
-import { MoonIcon } from 'lucide-react';
 import { Title } from '../Title';
-import { NavItem } from '../NavItem';
+import { useState, useEffect } from 'react';
+import { MoonIcon, SunIcon } from 'lucide-react';
 
-
+type AvailableThemes = 'dark' | 'light'
 
 export function NavBar() {
+
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'light';
+    return storageTheme
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon/>,
+    light: <MoonIcon/>
+  }
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    })
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
   return (
     <>
       <div id='navbar-section' className={styles.navbar}>
         <Title>Fernando Dalpiaz</Title>
         <div className={styles.navitems}>
-          <NavItem>Projetos</NavItem>
-          <NavItem>Formação</NavItem>
-          <NavItem>Contato</NavItem>
-          <NavItem><MoonIcon/></NavItem>
+          <a href='#' className={styles.item}>Projetos</a>
+          <a href='#' className={styles.item}>Formação</a>
+          <a href='#' className={styles.item}>Contato</a>
+          <a  onClick={handleThemeChange} href='#' className={styles.item}>
+            {nextThemeIcon[theme]}
+          </a>
         </div>
       </div>
     </>
