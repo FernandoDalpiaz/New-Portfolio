eae0f5a0b06da918144712ee947e91dae984a184
Fernando Dalpiaz <fernandoldalpiaz@gmail.com>
2026-02-19 11:10:23 -0300

    Build final da primeira versão

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -8,9 +8,9 @@ export function Contact() {
       <div id='contact-section' className={styles.contact}>
           <Title>Contato</Title>
           <div className={styles['contact-btns']}>
-            <Button>Linkedin</Button>
-            <Button>Github</Button>
-            <Button>Email</Button>
+            <Button href='https://www.linkedin.com/in/fernando-dalpiaz' target="_blank" rel="noopener noreferrer">Linkedin</Button>
+            <Button href='https://github.com/FernandoDalpiaz' target="_blank" rel="noopener noreferrer">Github</Button>
+            <Button href='mailto:fernandoldalpiaz@gmail.com' target="_blank" rel="noopener noreferrer">Email</Button>
           </div>
       </div>
     </>
